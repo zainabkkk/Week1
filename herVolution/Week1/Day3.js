9b4aa72f3a524349b1351e650464c3de80fcc437
zainabkkk <62759901+zainabkkk@users.noreply.github.com>
2020-08-03 22:39:41 -0400

    Create Day3.js

---
+++ herVolution/Week1/Day3.js
@@ -0,0 +1,52 @@
+/*Write a program that prints the first n elements of an array. */
+
+let example = ["A", "B", "C"];
+console.log(names[0], names [1])
+
+/* the number 1 can be switched to any value n */
+
+
+
+/*
+Let’s try 5 array operations.
+Create an array styles with items “Jazz” and “Blues”.
+Append (add) “Rock-n-Roll” to the end.
+Replace the value in the middle by “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+Strip off the first value of the array and show it.
+Prepend Rap and Reggae to the array.
+Console.log every step of the way
+*/
+
+
+let array = ["Jazz", "Blues"]
+array.push('Rock-n-Roll');
+console.log(array);
+//To remove just one element from an array at a certain index:
+array.splice(1, "Classics");
+console.log(array);
+array.shift(); 
+console.log(array);
+fruits.unshift('Rap', "Reggae"); // add to the front
+console.log(array);
+// ["Strawberry", "Banana"] 2
+
+
+/*
+Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object. (This one is a bit tricky and you will need to look it up - wasnt able to do this).
+Console log every step of the way.
+*/
+
+
+let empty = {
+    name: "John",
+    surname: "Smith",
+    name: "Pete",
+ 
+};
+
